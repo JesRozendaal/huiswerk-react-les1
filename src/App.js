763cec376de4a1bb4d6ac6c229878481fdc1d430
Jessica Rozendaal <remyenjessica@gmail.com>
2022-02-25 12:37:10 +0100

    producten-gemaakt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
-import Buttons from "./Buttons";
+import Buttons from './components/Buttons';
+import Products from './components/Products';
+import bag_1 from './assets/bag_1.png';
+import bag_2 from './assets/bag_2.png';
+import bag_3 from './assets/bag_3.png';
+import bag_4 from './assets/bag_4.png';
 
 function App() {
   return (
-      <div>
+      <>
       <h1>Handbags & Purses</h1>
         <nav>
           <Buttons
-          buttonName="to the collection"
+            buttonName="to the collection"
           />
         <Buttons
           buttonName="shop all bags"
@@ -18,7 +23,33 @@ function App() {
             disabled="true"
             />
         </nav>
-      </div>
+        <main>
+          <Products
+            category="Best seller"
+            image={bag_1}
+            text="The handy bag"
+            price="€400,-"
+          />
+          <Products
+            category="Best seller"
+            image={bag_2}
+            text="The stylish bag"
+            price="€250,-"
+            />
+          <Products
+              category="New collection"
+              image={bag_3}
+              text="The simple bag"
+              price="€300,-"
+              />
+          <Products
+              category="New collection"
+              image={bag_4}
+              text="The trendy bag"
+              price="€150,-"
+          />
+        </main>
+      </>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Products ({category,  image, text, price}) {
+    return (
+        <article>
+            <span>{category}</span>
+            <img src={image} alt={text}/>
+            <p>{text}</p>
+            <h4>{price}</h4>
+        </article>
+    )
+}
+
+export default Products;
